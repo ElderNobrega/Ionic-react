c01c61507ecab216bfb505efb10196175759b317
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-01-12 22:49:57 -0500

    adding new router

--- Ionic-Test/src/pages/Home.tsx
+++ Ionic-Test/src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge, IonFab, IonFabButton, IonIcon } from '@ionic/react';
 import React from 'react';
+import { add } from 'ionicons/icons';
+import { RouteComponentProps } from 'react-router';
 
-const Home: React.FC = () => {
+
+const Home: React.FC<RouteComponentProps> = (props) => {
   return (
     <IonPage>
       <IonHeader>
@@ -9,15 +12,24 @@ const Home: React.FC = () => {
           <IonTitle>Ionic Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonCheckbox slot="start" />
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run idea by Brandy</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              5 days
+            </IonBadge>
+          </IonItem>
+        </IonList>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => props.history.push('/new')}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
